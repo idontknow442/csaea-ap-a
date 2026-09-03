026ef873a455afa05c42ca00aa05d4b187fff1f3
idontknow442 <singh20288804@student.nhvweb.net>
2026-09-03 13:38:13 +0000

    change2

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -1,66 +1,108 @@
 package CrashCourse;
+// If your Java file isn't running, you *might* need to add the following line and uncomment it, at the top of your file: 
+// package CrashCourse (the name of the folder that includes CrashCourse.java)
 
-public class CrashCourse {
-    
+public class CrashCourse {     // class header. Always PascalCase. 
+
     public static void main(String[] args) {
 
-        // comments
+        // comments 
+        
+        /*  Multiline 
+         Comment
+         Here
+         */
 
-        /* Multiline
-            Comment
-        */
+        System.out.print("Hello");
+        System.out.println("World"); // prints on a new line
+        System.out.println("Hello world");
 
-        System.out.print("hello");
-        System.out.println("World"); // auto prints on new line
-        System.out.println("Hello World");
+        // VARIABLE DECLARATIONS:
+        // primitive variables:
 
-        // variable declarations:
-        // PRIMITIVE VARIABLES
         int a; // integer
-        double b; // decimal/float
-        boolean c; // true or false
-
+        double b; // decimal, or float
+        boolean c; // true or false;
+        
         a = 4;
         b = 5.5;
         c = false; // lowercase
 
-        // arithmetic operations
-        // + - / * %
-        // += -= /= %=
+        // Arithmetic Operators 
+        // + - / *  %
+        // += -= /=  %=  (compound assignment operators)
 
-        int d = 3;
-        d += 7;
+        int d = 3 - 1; // d => 2
+        d += 7; // d => 9
 
-        System.out.println("d = " + d);
+        System.out.println("d is equal to " + d);
 
         // increment and decrement by one:
-        // -- ++
-        d--;
-        d++;
-        d++;
-        System.out.println("d is NOW equal to " + d);
+        // --  ++
+        d--; // 9 - 1 = 8
+        d--; // 8 - 1 = 7
+        d++; // 7 + 1 = 8
+        System.out.println("d is NOW equal to " + d); // => 8
 
-        // COMPARISONS (always returns true or false)
-        // <> <= >= == !=
+        //COMPARISONS (always returns true or false)
+        //  <  >   <=   >=   ==   !=
         System.out.println(4 < 5);
         System.out.println(7 == 4);
-        System.out.println(7 != 2);
+        System.out.println(1 != 2);
 
-        // LOGICAL OPERATORS
-        // In order of precedence: !(not) &&(and) ||(or)
+        // LOGICAL OPERATORS 
+        // In order of presedence: NOT:  !   AND: &&   OR: ||
 
-        boolean e = false; boolean f = true;
+        boolean f = false; boolean t = true;
+
+        // predict output: true or false? 
+        System.out.println(!f);  //true
+        System.out.println(f && t);// false
+        System.out.println(f || t); //true
+        System.out.println(f || t && !f); // true
+
+        System.out.println(f && t); // short circuits to FALSE after checking f && -- java does not need to check t.
+        System.out.println(t || (f && t)); //short circuits after checking the first t.
+
+        // CASTING (converting)
+        int g = (int) 5.5;
+        System.out.println(g);
+
+        double h = (double) 5/6;
+        System.out.println(h); 
+
+        // STRINGS
+        String s1 = "Goodnight";
+        String s2 = " and ";
+        String s3 = "Goodbye";
+        String result = s1 + s2 + s3;
+        result += ", Cowboy.";
+        System.out.println(result + "\n");
         
-        // predict output true or false?
-        System.out.println(!e); // false
-        System.out.println(e && f); // false
-        System.out.println(e || f);  // true
-        System.out.println(e || f && !e); // true
+        //
+        int[] arry1 = new int[10];
+        System.out.println(arry1);
+        System.out.println("length: " + arry1.length);
 
-        System.out.println(e && f); // short circuits after checking e -- java does not need to check f
+        arry1[0] = 11;
+        arry1[1] = 2;
+
+        System.out.println("Remainder: " + arry1[0] % arry1[1]);
+
+        int[] arry2 = {34, 52, 3, 64, 32};
+        System.out.println("arry2: " + arry2);
+
+        // 2D arrays (grid or a table)
+
+        int[][] arryGrid1 = new int[4][3]; // 4 rows, 3 columns
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+
+        System.out.println("Rows: " + arryGrid1.length);
+        System.out.println("Columns: " + arryGrid1[0].length);
 
 
     }
-    
-
 }
