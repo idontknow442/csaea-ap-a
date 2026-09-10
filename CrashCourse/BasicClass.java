1c0436179e791f1ef2866b90a4cc06fcbce9b479
idontknow442 <singh20288804@student.nhvweb.net>
2026-09-10 13:08:16 +0000

    added basic class

---
+++ CrashCourse/BasicClass.java
@@ -0,0 +1,19 @@
+package CrashCourse;
+
+public class BasicClass {
+    
+    public static void main(String[] args) {
+
+        // attributes / data
+        // variables
+
+        // Constructor --- SPECIAL METHOD called everytime you make an object
+        // Same name as the class, followed by parentheses
+        // BasicClass()
+
+        // behaviors / methods
+        // you can write as many as you want
+
+    }
+
+}
